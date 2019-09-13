8bb872ea5edd9f12ff9917cb2417bca22466fa04
R-second <R-second@yamadarigatsu-no-MacBook-Air.local>
2019-09-13 20:18:12 +0900

    changed from sqlite3 into mysql

--- databaseMethod.py
+++ databaseMethod.py
@@ -1,37 +1,51 @@
 
-import sqlite3
+import mysql.connector
 
 ###  データベース関連メソッド集
+def db_connect():
+    connect = mysql.connector.connect(
+        host='localhost',
+        port='8889',
+        user='root',
+        password='root',
+        database='shopdb'
+    )
+    return connect
+
+
 # shop.dbにデータを登録
 def setDataShop(name, price, junre, stock, reservation):
-    conn = sqlite3.connect("shop.db")  # データベースに接続
-    cur = conn.cursor()
-    sql = "INSERT INTO Shop (name, price, junre, stock, reservation) VALUES (?, ?, ?, ?, ?) "
+    connect = db_connect()
+    cur = connect.cursor()
+    sql = "INSERT INTO Shop (name, price, junre, stock, reservation) VALUES (%s, %s, %s, %s, %s) "
     cur.execute(sql, (name, price, junre, stock, reservation))
-    conn.commit()
+    connect.commit()
 
 # reservation.dbにデータを登録
 def setDataReservation(grade, department, number, name, purchase, price, password):
-    conn = sqlite3.connect("shop.db")  # データベースに接続
-    cur = conn.cursor()
-    sql = "INSERT INTO Reservation (grade, department, number, name, purchase, price, password) VALUES (?, ?, ?, ?, ?, ?, ?) "
+    connect = db_connect()
+    cur = connect.cursor()
+    sql = "INSERT INTO Reservation (grade, department, number, name, purchase, price, password) VALUES (%s, %s, %s, %s, %s, %s, %s) "
+
     cur.execute(sql, (grade, department, number, name, purchase, price, password))
-    conn.commit()
+    connect.commit()
 
 def deleteData(database, id):
-    conn = sqlite3.connect("shop.db")  # データベースに接続
-    cur = conn.cursor()
-    sql = "DELETE FROM " + database + "WHERE id = " + id
+    connect = db_connect()
+    cur = connect.cursor()
+    sql = "DELETE FROM " + database + "WHERE id = " + str(id)
     cur.execute(sql)
-    conn.commit()
+    connect.commit()
+
+
 
 
 # databaseからcolumn列の情報を取得する
 # id指定も可能
 # columnの場所を * にすることで全体の取得も可能
 def getDBInfo(database, column, id=None):
-    conn = sqlite3.connect("shop.db")  # データベースに接続
-    cur = conn.cursor()
+    connect = db_connect()
+    cur = connect.cursor()
     data = []
     if(id == None):  # idが指定されていない場合
         sql = " SELECT " + column + " FROM " + database
@@ -39,7 +53,6 @@ def getDBInfo(database, column, id=None):
         sql = " SELECT " + column + " FROM "+database + " WHERE id= " + str(id)
     cur.execute(sql)
 
-    conn.commit()
     for row in cur:
         data.append(row)
     return data
