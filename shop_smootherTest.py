9531f9d0e077573b271fcfcf486e64ae1f166933
R-second <R-second@yamadarigatsu-no-MacBook-Air.local>
2019-09-27 17:28:15 +0900

    to test

--- shop_smootherTest.py
+++ shop_smootherTest.py
@@ -1,3 +1,4 @@
+
 # shop.py
 
 import sqlite3
@@ -11,5 +12,6 @@ ps = password.encryptPassword(ps)
 ps_raw = repr(ps)
 
 databaseMethod.setDataReservation(2, 1, 32, "山田", "000000", 150,  ps_raw)
+cipher_data = eval(databaseMethod.getDBInfo("reservation", "password", 1)[0][0] )   # getDBInfoメソッドを使ってidのパスワード(暗号化済み)を取得
 
-print(password.decryptPassword(1))
+print(password.decryptPassword(cipher_data))
